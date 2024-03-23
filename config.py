48467869ad2fdd011aabf8f514c6c5cbafb4d004
Lekuru <contact@lekuru.xyz>
2024-03-24 00:00:31 +0100

    Add base config

---
+++ config.py
@@ -0,0 +1,17 @@
+
+import dotenv
+import os
+
+dotenv.load_dotenv(override=True)
+
+# Load environment variables
+REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
+REDIS_PORT = os.getenv("REDIS_PORT", 6379)
+REDIS_PASSWORD = os.getenv("REDIS_PASSWORD", None)
+REDIS_DB = os.getenv("REDIS_DB", 0)
+
+POSTGRES_HOST = os.getenv("POSTGRES_HOST", "localhost")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT", 5432)
+POSTGRES_USER = os.getenv("POSTGRES_USER", "postgres")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "postgres")
+POSTGRES_DB = POSTGRES_USER
