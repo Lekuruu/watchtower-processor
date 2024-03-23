48597942524d33fc7dd342e7af51860b2be9dfc5
Lekuru <contact@lekuru.xyz>
2024-03-24 00:19:45 +0100

    Add postgres object

--- app/session.py
+++ app/session.py
@@ -1,8 +1,15 @@
 
-from app.data import EventQueue
+from app.data import EventQueue, Postgres
 from redis import Redis
 
 import config
+
+postgres = Postgres(
+    config.POSTGRES_USER,
+    config.POSTGRES_PASSWORD,
+    config.POSTGRES_HOST,
+    config.POSTGRES_PORT
+)
 
 redis = Redis(
     config.REDIS_HOST,
