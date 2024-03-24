30227e80ff009e117cc782e1acfb0ccb5737d7b4
Lekuru <contact@lekuru.xyz>
2024-03-24 14:44:04 +0100

    Implement score processor

--- app/processor.py
+++ app/processor.py
@@ -2,19 +2,59 @@
 from app.data import DBMessage, DBScore
 from app.data import messages, scores
 
+import app.performance as performance
 import app.session as session
+import json
 
 @session.events.register("score")
 def process_score(
     checksum: str,
     server: str,
-    player: dict,
-    score: dict
-):
-    session.logger.info(
-        f"Processing score from {player['name']} on {server} ({checksum})..."
+    player: str,
+    score: str
+) -> None:
+    player = json.loads(player)
+    score = json.loads(score)
+
+    pp = performance.calculate_ppv2(
+        player['status']['beatmap_id'],
+        player['status']['mode'],
+        score
+    ) or None
+
+    score = scores.create(
+        DBScore(
+            user_id=player['id'],
+            user_name=player['name'],
+            user_country=player['country'],
+            server=server,
+            beatmap_id=player['status']['beatmap_id'],
+            beatmap_text=player['status']['text'],
+            beatmap_checksum=player['status']['checksum'],
+            checksum=checksum,
+            mode=player['status']['mode'],
+            pp=pp,
+            acc=score['accuracy'],
+            total_score=score['total_score'],
+            max_combo=score['max_combo'],
+            mods=score['mods'],
+            perfect=score['perfect'],
+            passed=score['passed'],
+            length=score['length'],
+            grade=score['grade'],
+            c300=score['c300'],
+            c100=score['c100'],
+            c50=score['c50'],
+            cgeki=score['cGeki'],
+            ckatu=score['cKatu'],
+            cmiss=score['cMiss'],
+            replay_filename=score['filename_safe']
+        )
     )
-    # TODO
+
+    session.logger.info(
+        f"Processed score from {player['name']} on {server} ({checksum})."
+    )
 
 @session.events.register("message")
 def process_message(
@@ -23,7 +63,7 @@ def process_message(
     sender_id: int,
     message: str,
     target: str
-):
+) -> None:
     messages.create(
         DBMessage(
             sender_id=sender_id,
@@ -33,6 +73,7 @@ def process_message(
             message=message
         )
     )
+
     session.logger.info(
         f"[#spectator-{target}] {target}: {message}"
     )
